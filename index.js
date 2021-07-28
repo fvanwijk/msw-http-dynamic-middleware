af1bef333209f2af0a140655c45b0ab63d05e030
Frank van Wijk <fvanwijk@xebia.com>
2021-07-28 14:56:15 +0200

    Add logging and fix setting scenario with multiple handlers

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+import { pino } from 'pino';
 import { rest } from 'msw';
+
+const logger = pino({ prettyPrint: {
+  translateTime: true,
+  ignore: 'pid,hostname'
+} });
 
 const defaultResolver = (req, res, ctx) => res(ctx.status(200));
 
@@ -25,10 +31,17 @@ export const createHandlers = scenarios => {
       const handlers = Array.isArray(handler) ? handler : [handler];
       
       return handlers.map(handler => {
-        const { method, path } = handler.info;
+        const { method, path, header } = handler.info;
         return rest[method.toLowerCase()](path, (req, res, ctx) => {
           // Forward call to active resolver that comes from scenario or fall back to default resolver
-          const resolver = activeResolvers[path]?.[method] || defaultResolver;
+          let resolver = activeResolvers[path]?.[method];
+          
+          if (!resolver) {
+            resolver = defaultResolver
+            logger.info(`${header} (default resolver)`);
+          } else {
+            logger.info(header);
+          }
   
           return resolver(req, res, ctx);
         });
@@ -46,18 +59,25 @@ export const createHandlers = scenarios => {
         );
       }
 
-      const selectedScenario = scenarios[scenarioName];
-      if (!selectedScenario) {
+      const handler = scenarios[scenarioName];
+      if (!handler) {
         return res(ctx.status(400), ctx.text(`Scenario "${scenarioName}" does not exist`));
       }
+      
+      const handlers = Array.isArray(handler) ? handler : [handler];
 
-      const { path, method } = selectedScenario.info;
-
-      if (!(path in activeResolvers)) {
-        activeResolvers[path] = {};
-      }
-
-      activeResolvers[path][method] = selectedScenario.resolver;
+      const headers = handlers.map(handler => {
+        const { path, method, header } = handler.info;
+  
+        if (!(path in activeResolvers)) {
+          activeResolvers[path] = {};
+        }
+  
+        activeResolvers[path][method] = handler.resolver;
+        return header;
+      })
+      
+      logger.info(`Set scenario "${scenarioName}" with resolvers for endpoints: ${headers.join(', ')}`)
 
       return res(ctx.status(205));
     }),
