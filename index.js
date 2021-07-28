cea639e4655516ba475ddb33eee6551b483467e7
Frank van Wijk <fvanwijk@xebia.com>
2021-07-28 11:17:42 +0200

    - Separate server from lib - Handle error when scenario does not exist

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
-import { createServer } from '@mswjs/http-middleware'
-import { createHandlers } from './handlers.js'
+import { rest } from 'msw'
 
-const scenarios = {
-  'user success': (req, res, ctx) => res(ctx.json({ name: 'frank' }))
+const methods = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'];
+const defaultHandler = (req, res, ctx) => res(ctx.status(200));
+
+const createError = (res, ctx, fieldName) => {
+  return res(ctx.status(401), ctx.text(`Please provide a ${fieldName} in the request body. Example: { "method": "GET", "path": "/user", "scenario": "user" }`));
 }
-  
-const endpoints = [
-  ['GET', '/user'],
-  ['GET', '/users']
-];
 
-const handlers = createHandlers(endpoints, scenarios);
+export const createHandlers = (endpoints, scenarios) => {
+  const activeHandlers = {};
 
-const httpServer = createServer(...handlers)
+  return [
+    ...endpoints.map(([method, endpoint]) => {
+      if (!methods.includes(method)) {
+        throw new Error(`Invalid input method ${method} provided. Valid methods: ${methods.join(', ')}`);
+      }
 
-httpServer.listen(9090)
+      return rest[method.toLowerCase()](endpoint, (req, res, ctx) => {
+        return activeHandlers[endpoint]?.GET(req, res, ctx) || defaultHandler(req, res, ctx)
+      })
+    }),
+
+    // Set mock for any endpoint
+    rest.put('/scenario', (req, res, ctx) => {
+      const { method, path, scenario} = req.body;
+
+      if (!method) {
+        return createError(res, ctx, 'method')
+      }
+      if (!path) {
+        return createError(res, ctx, 'path')
+      }
+      if (!scenario) {
+        return createError(res, ctx, 'scenario');
+      }
+
+      if (!(path in activeHandlers)) {
+        activeHandlers[path] = {};
+      }
+
+      const selectedScenario = scenarios[scenario];
+      if (!selectedScenario) {
+        return res(ctx.status(401), ctx.text(`Scenario "${scenario}" does not exist`))
+      };
+
+      activeHandlers[path][method] = selectedScenario;
+
+      return res(ctx.status(205));
+    })
+  ]
+};
