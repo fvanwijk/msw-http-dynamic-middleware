c566fab79fc97861a3aa28b65c8a46299b1ea752
Frank van Wijk <fvanwijk@xebia.com>
2021-07-31 15:09:32 +0200

    Add some styling using TailwindCSS

---
+++ msw-ui/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{js,ts}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
